546293918ee128b7f1ec954dbcb71b0558582eae
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-05 14:45:34 +0200

    Add the DOM of the dataset.home of the close-svg inside the menuSlider function

--- src/menu-slide.js
+++ src/menu-slide.js
@@ -1,17 +1,12 @@
 const menuBtn = document.querySelector(".menu");
 const sideBarSection = document.querySelector(".sideBar-section");
-const calenderClockSection = document.querySelector(".calender-clock-section");
-const taskSection = document.querySelector(".task-section");
-const addTaskBtnSection = document.querySelector(".add-task-btn-section");
+const closeModalSvg = document.querySelector(".close-svg");
 
 // This function is for showing the sideBar section and hide
 // the other sections
 export function menuSlider() {
   menuBtn.addEventListener("click", function () {
     sideBarSection.classList.toggle("slide-sideBar");
-
-    calenderClockSection.classList.toggle("hidden");
-    taskSection.classList.toggle("hidden");
-    addTaskBtnSection.classList.toggle("hidden");
+    closeModalSvg.dataset.home = "false";
   });
 }
