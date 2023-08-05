649ddfa4218009a025e5568028a468d6aacc8095
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-05 10:42:39 +0200

    Add comment to the file

--- src/menu-slide.js
+++ src/menu-slide.js
@@ -3,6 +3,9 @@ const sideBarSection = document.querySelector(".sideBar-section");
 const calenderClockSection = document.querySelector(".calender-clock-section");
 const taskSection = document.querySelector(".task-section");
 const addTaskBtnSection = document.querySelector(".add-task-btn-section");
+
+// This function is for showing the sideBar section and hide
+// the other sections
 export function menuSlider() {
   menuBtn.addEventListener("click", function () {
     sideBarSection.classList.toggle("slide-sideBar");
