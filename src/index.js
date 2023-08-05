309e2cabdc4ecd006b751e4f48d151cdcaf72819
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-05 14:24:23 +0200

    Import the closeAddTaskModal function and invoked

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import { menuSlider } from "./menu-slide.js";
+import { closeAddTaskModal } from "./add-task-modal.js";
 
 document.querySelector("body").classList.add("dayBackground");
 
 menuSlider();
+closeAddTaskModal();
