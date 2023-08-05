43b1541024164796d10aaabf58afb26179ca7fae
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-05 14:46:51 +0200

    Modified the closeAddTaskModal function to do DOM with the dataset.home attribute of the close-svg. Create the openAddTaskModalHome function

--- src/add-task-modal.js
+++ src/add-task-modal.js
@@ -5,8 +5,23 @@ const sideBarSection = document.querySelector(".sideBar-section");
 
 export function closeAddTaskModal() {
   closeModalSvg.addEventListener("click", function () {
-    addTaskModalSection.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
-    sideBarSection.classList.add("slide-sideBar");
+    if (closeModalSvg.dataset.home === "true") {
+      overlay.classList.add("hidden");
+      addTaskModalSection.classList.add("hidden");
+    } else {
+      addTaskModalSection.classList.toggle("hidden");
+      overlay.classList.toggle("hidden");
+      sideBarSection.classList.add("slide-sideBar");
+    }
+  });
+}
+
+const addTaskHomeBtn = document.querySelector(".add-task-svg-box");
+
+export function openAddTaskModalHome() {
+  addTaskHomeBtn.addEventListener("click", function () {
+    closeModalSvg.dataset.home = "true";
+    overlay.classList.remove("hidden");
+    addTaskModalSection.classList.remove("hidden");
   });
 }
