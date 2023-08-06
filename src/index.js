d6ee7aecb27819948a974e0f6f73edb5b1fb4fe3
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-06 20:56:08 +0200

    Import and invoke the clockFunction inside the SetInterval function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import "./style.css";
 import { menuSlider } from "./menu-slide.js";
 import { closeAddTaskModal } from "./add-task-modal.js";
 import { openAddTaskModalHome } from "./add-task-modal.js";
-
+import { clockFunction } from "./clock.js";
 document.querySelector("body").classList.add("dayBackground");
 
 menuSlider();
 closeAddTaskModal();
 openAddTaskModalHome();
+// Clock-function ↓
+clockFunction();
+setInterval(function () {
+  clockFunction();
+}, 60000);
+// *********
