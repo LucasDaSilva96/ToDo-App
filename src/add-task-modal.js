599529801e4cd474bc2dc295dc08fd0054f805f8
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-08 13:39:27 +0200

    Create the openAddTaskModalSideBar function

--- src/add-task-modal.js
+++ src/add-task-modal.js
@@ -5,9 +5,12 @@ const sideBarSection = document.querySelector(".sideBar-section");
 
 export function closeAddTaskModal() {
   closeModalSvg.addEventListener("click", function () {
-    if (closeModalSvg.dataset.home === "true") {
+    if (closeModalSvg.dataset.navigation === "home") {
       overlay.classList.add("hidden");
       addTaskModalSection.classList.add("hidden");
+    } else if (closeModalSvg.dataset.navigation === "sideBar") {
+      addTaskModalSection.classList.toggle("hidden");
+      overlay.classList.toggle("hidden");
     } else {
       addTaskModalSection.classList.toggle("hidden");
       overlay.classList.toggle("hidden");
@@ -20,7 +23,17 @@ const addTaskHomeBtn = document.querySelector(".add-task-svg-box");
 
 export function openAddTaskModalHome() {
   addTaskHomeBtn.addEventListener("click", function () {
-    closeModalSvg.dataset.home = "true";
+    closeModalSvg.dataset.navigation = "home";
+    overlay.classList.remove("hidden");
+    addTaskModalSection.classList.remove("hidden");
+  });
+}
+
+const addTaskSideBarBtn = document.querySelector(".sideBar-add-task-btn");
+
+export function openAddTaskModalSideBar() {
+  addTaskSideBarBtn.addEventListener("click", function () {
+    closeModalSvg.dataset.navigation = "sideBar";
     overlay.classList.remove("hidden");
     addTaskModalSection.classList.remove("hidden");
   });
