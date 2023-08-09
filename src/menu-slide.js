36cf9bd77a9616f41cd36f591ccade773e392542
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-09 23:57:09 +0200

    Import and invoke the renderProjects function inside of the menuSlider function

--- src/menu-slide.js
+++ src/menu-slide.js
@@ -2,10 +2,13 @@ const menuBtn = document.querySelector(".menu");
 const sideBarSection = document.querySelector(".sideBar-section");
 const closeModalSvg = document.querySelector(".close-svg");
 
+import { renderProjects } from "./storage.js";
+
 // This function is for showing the sideBar section and hide
 // the other sections
 export function menuSlider() {
   menuBtn.addEventListener("click", function () {
+    renderProjects();
     sideBarSection.classList.toggle("slide-sideBar");
 
     if (sideBarSection.classList.contains("slide-sideBar")) {
