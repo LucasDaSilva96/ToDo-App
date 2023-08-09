d66eac698f3599e2d71900a74568d15c99a19a1d
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-09 23:55:41 +0200

    Create the PROJECT and the TASK class. Create the following functions: - storage - projectsExist

--- src/classes.js
+++ src/classes.js
@@ -5,13 +5,57 @@ export class PROJECT {
   }
 
   saveToLocalStorage() {
-    const counter = window.localStorage.length;
     const obj = {
       project_name: this.projectName,
       project_notis: null,
       project_tasks: [],
     };
-    const str = JSON.stringify(obj);
-    localStorage.setItem(`Project_${counter}`, str);
+    storage(obj);
+  }
+}
+
+export class TASK {
+  constructor(projectName, task_title, task_message, task_due, task_priority) {
+    this.projectName = projectName;
+    this.task_title = task_title;
+    this.task_message = task_message;
+    this.task_due = task_due;
+    this.task_priority = task_priority;
+  }
+
+  saveTaskToProject() {
+    const obj = {
+      title: this.task_title,
+      message: this.task_message,
+      due: this.task_due,
+      priority: this.task_priority,
+      done: false,
+      remove: false,
+    };
+    const data_0 = window.localStorage.getItem("Projects");
+    const data = JSON.parse(data_0);
+    const index = data.projects.findIndex(
+      (element) => element.project_name === this.projectName
+    );
+    data.projects[index].project_tasks.push(obj);
+    return window.localStorage.setItem("Projects", JSON.stringify(data));
+  }
+}
+
+function storage(project) {
+  let data = JSON.parse(window.localStorage.getItem("Projects"));
+
+  data.projects.push(project);
+
+  return localStorage.setItem("Projects", JSON.stringify(data));
+}
+
+export function projectsExist() {
+  const projects = window.localStorage.getItem("Projects");
+
+  if (projects === null || projects === undefined) {
+    return false;
+  } else {
+    return true;
   }
 }
