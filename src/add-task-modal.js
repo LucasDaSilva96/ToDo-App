889ba147fb2a1416c00916a50dae41d22f05cc3d
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-08-10 15:25:24 +0200

    Create the following functions: - isProjectSelected - isPrioritySelected - isTaskTitleLegit - changePrioritySelected - padTo2Digits - formatDate - changeCalenderValue - addTaskFunction - checkForTitleDuplicate

--- src/add-task-modal.js
+++ src/add-task-modal.js
@@ -1,9 +1,12 @@
+import { projectsExist } from "./classes.js";
+import { renderProjectSelections } from "./storage.js";
+import { TASK } from "./classes.js";
+
 const addTaskModalSection = document.querySelector(".add-task-modal-section");
 const overlay = document.querySelector(".overlay");
 const closeModalSvg = document.querySelector(".close-svg");
 const sideBarSection = document.querySelector(".sideBar-section");
-import { projectsExist } from "./classes.js";
-import { renderProjectSelections } from "./storage.js";
+let newTask;
 
 export function closeAddTaskModal() {
   closeModalSvg.addEventListener("click", function () {
@@ -72,4 +75,125 @@ export function showAddProjectInput() {
     projectInputField.textContent = "";
     projectInputField.value = "";
   });
+}
+
+const selectProjectHtml = document.getElementById("project-selection");
+const priorityDivs = document.querySelectorAll(".priority");
+const taskTitle = document.querySelector(".title");
+const taskMessage = document.getElementById("message");
+function isProjectSelected() {
+  if (selectProjectHtml.value === "Select Project") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+let prioritySelected;
+
+function isPrioritySelected() {
+  let isLegit = false;
+
+  priorityDivs.forEach((el) => {
+    if (el.dataset.selected === "true") {
+      isLegit = true;
+      prioritySelected = el.id;
+    }
+  });
+
+  return isLegit;
+}
+
+function isTaskTitleLegit() {
+  if (taskTitle.value === "" || taskTitle.value === " ") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export function changePrioritySelected() {
+  priorityDivs.forEach((el) => {
+    el.addEventListener("click", function () {
+      if (el.dataset.selected === "false") {
+        el.dataset.selected = "true";
+      } else if (el.dataset.selected === "true") {
+        el.dataset.selected = "false";
+      }
+    });
+  });
+}
+
+const dueDateCalender = document.getElementById("calender");
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, "0");
+}
+
+function formatDate(date = new Date()) {
+  return [
+    date.getFullYear(),
+    padTo2Digits(date.getMonth() + 1),
+    padTo2Digits(date.getDate()),
+  ].join("-");
+}
+
+export function changeCalenderValue() {
+  dueDateCalender.value = formatDate();
+  dueDateCalender.addEventListener("change", function () {
+    const selectedDate = dueDateCalender.value;
+    return (dueDateCalender.value = selectedDate);
+  });
+}
+
+const addTaskBtn = document.querySelector(".add-task-box");
+
+export function addTaskFunction() {
+  // ****
+  let project = undefined;
+  selectProjectHtml.addEventListener("change", function () {
+    project = selectProjectHtml.value;
+  });
+  // ***
+
+  addTaskBtn.addEventListener("click", function () {
+    if (isPrioritySelected() && isProjectSelected() && isTaskTitleLegit()) {
+      newTask = new TASK(
+        project,
+        taskTitle.value,
+        taskMessage.value,
+        dueDateCalender.value,
+        prioritySelected
+      );
+
+      if (checkForTitleDuplicate(taskTitle.value, project)) {
+        newTask.saveTaskToProject();
+      } else {
+        window.alert("You already have this title on the selected project");
+      }
+    } else {
+      return;
+    }
+  });
+}
+
+function checkForTitleDuplicate(title, projectName) {
+  const data = window.localStorage.getItem("Projects");
+  const data_1 = JSON.parse(data);
+
+  const index_proj = data_1.projects.findIndex((el) => {
+    return el.project_name === projectName;
+  });
+
+  const index_title = data_1.projects[index_proj].project_tasks.findIndex(
+    (el) => {
+      return el.title === title;
+    }
+  );
+
+  if (index_title < 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
